2eb6d7430d55c0e64c27a478143c2d817ad1cfad
tgyuuAn <atg0614@gmail.com>
2023-04-07 22:38:01 +0900

    add splashFragment

--- app/src/main/java/com/android/mediproject/MainActivity.kt
+++ app/src/main/java/com/android/mediproject/MainActivity.kt
@@ -1,31 +1,12 @@
 package com.android.mediproject
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.android.mediproject.core.ui.base.BaseActivity
 import com.android.mediproject.databinding.ActivityMainBinding
 
-class MainActivity() : AppCompatActivity(){
+class MainActivity() : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
-    private lateinit var binding:  ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.activity_main,null,false)
-        setContentView(binding.root)
-    }
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-    }
-
-    override fun onStop() {
-        super.onStop()
+    override fun afterBinding() {
+        TODO("Not yet implemented")
     }
 }
