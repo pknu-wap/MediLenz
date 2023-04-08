f34858d986fbc1fd5c034134e63956f3bf635679
tgyuu-An <atg0614@gmail.com>
2023-04-08 12:35:03 +0900

    add intro

---
+++ feature/intro/src/main/java/com/android/mediproject/feature/intro/IntroFragment.kt
@@ -0,0 +1,31 @@
+package com.android.mediproject.feature.intro
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.android.mediproject.core.ui.base.BaseFragment
+import com.android.mediproject.feature.intro.databinding.FragmentIntroBinding
+import repeatOnStarted
+
+class SplashFragment : BaseFragment<FragmentIntroBinding, IntroViewModel>(FragmentIntroBinding::inflate) {
+
+    override val fragmentViewModel: IntroViewModel by viewModels()
+
+    override fun afterBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) {
+        binding.viewModel = fragmentViewModel.apply{
+            repeatOnStarted { eventFlow.collect{handelEvent(it)} }
+        }
+    }
+
+    fun handelEvent(event : IntroViewModel.IntroEvent) = when(event){
+        is IntroViewModel.IntroEvent.NonMemberLogin -> {}
+        is IntroViewModel.IntroEvent.MemberLogin-> {}
+        is IntroViewModel.IntroEvent.SignUp -> {}
+    }
+
+}
---
+++ feature/intro/src/main/java/com/android/mediproject/feature/intro/IntroViewModel.kt
@@ -0,0 +1,25 @@
+package com.android.mediproject.feature.intro
+
+import MutableEventFlow
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import asEventFlow
+import com.android.mediproject.core.ui.base.BaseViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class IntroViewModel : BaseViewModel() {
+    private val _eventFlow = MutableEventFlow<IntroEvent>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun event(event : IntroEvent) = viewModelScope.launch{ _eventFlow.emit(event)}
+    fun nonMemberLogin(){event(IntroEvent.NonMemberLogin())}
+    fun memberLogin(){event(IntroEvent.MemberLogin())}
+    fun signUp(){event(IntroEvent.SignUp())}
+
+    sealed class IntroEvent{
+        data class NonMemberLogin(val unit : Unit? = null) : IntroEvent()
+        data class MemberLogin(val unit : Unit? = null) : IntroEvent()
+        data class SignUp(val unit : Unit? = null) : IntroEvent()
+    }
+}
