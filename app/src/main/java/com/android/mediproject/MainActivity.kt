0092ce78fc4e868ac7bf1812177aba28f2fba1ed
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-04-30 21:45:44 +0900

    #6 MainActivity.kt내 불 필요한 코드 제거

--- app/src/main/java/com/android/mediproject/MainActivity.kt
+++ app/src/main/java/com/android/mediproject/MainActivity.kt
@@ -3,7 +3,6 @@ package com.android.mediproject
 import android.annotation.SuppressLint
 import com.android.mediproject.core.ui.base.BaseActivity
 import com.android.mediproject.databinding.ActivityMainBinding
-import com.android.mediproject.feature.home.HomeFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,14 +10,7 @@ class MainActivity() : BaseActivity<ActivityMainBinding>(ActivityMainBinding::in
     @SuppressLint("CommitTransaction")
     override fun afterBinding() {
         binding.apply {
-            /*
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-            val navController = navHostFragment.navController
-            bottomNav.setupWithNavController(navController)
 
-             */
-
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, HomeFragment()).commit()
         }
     }
 
