02ac8df8e35e4790b5439be62d23d732e8465a44
ho-sick99 <jhmoon6807@gmail.com>
2023-05-01 18:50:56 +0900

    #18 MCR 패턴 적용

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@
 // 모듈
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 // 라우팅
 const home = require("./src/routes/home");
 
 // 미들웨어
-app.use("/", home); 
+app.use("/", home);
 
 module.exports = app;
