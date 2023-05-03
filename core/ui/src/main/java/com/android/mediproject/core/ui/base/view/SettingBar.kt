5b76d6b3dea65033dc2f3609b94f0aab78456726
tgyuu-An <atg0614@gmail.com>
2023-05-03 21:37:13 +0900

    add baseSettingBar class logic

--- core/ui/src/main/java/com/android/mediproject/core/ui/base/view/SettingBar.kt
+++ core/ui/src/main/java/com/android/mediproject/core/ui/base/view/SettingBar.kt
@@ -1,4 +1,42 @@
 package com.android.mediproject.core.ui.base.view
 
-class SettingBar {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.AttrRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.android.mediproject.core.ui.R
+
+class SettingBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    lateinit var title: TextView
+    lateinit var arrow: ImageView
+
+    init {
+        val infService = Context.LAYOUT_INFLATER_SERVICE
+        val li = context.getSystemService(infService) as LayoutInflater
+        val view = li.inflate(R.layout.settingbar, this, false)
+        addView(view)
+
+        title = findViewById<TextView>(R.id.settingBarTV)
+        arrow = findViewById<ImageView>(R.id.settingBarIV)
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SettingBar)
+
+        title.text = typedArray.getString(R.styleable.SettingBar_title) ?: ""
+        arrow.visibility = when (typedArray.getBoolean(R.styleable.SettingBar_showArrow, true)) {
+            true -> View.VISIBLE
+            else -> View.INVISIBLE
+        }
+
+        typedArray.recycle()
+    }
+
 }
