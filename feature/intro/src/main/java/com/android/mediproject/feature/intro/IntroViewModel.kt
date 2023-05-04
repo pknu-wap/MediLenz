c704089cda569ab4d2aea36e8dfd192bd99d4b66
tgyuu-An <atg0614@gmail.com>
2023-05-04 13:53:43 +0900

    chore viewModel event함수 괄호 없앰

--- feature/intro/src/main/java/com/android/mediproject/feature/intro/IntroViewModel.kt
+++ feature/intro/src/main/java/com/android/mediproject/feature/intro/IntroViewModel.kt
@@ -13,15 +13,15 @@ class IntroViewModel @Inject constructor() : BaseViewModel() {
     private val _eventFlow = MutableEventFlow<IntroEvent>()
     val eventFlow = _eventFlow.asEventFlow()
 
-    fun event(event : IntroEvent) = viewModelScope.launch{ _eventFlow.emit(event)}
+    fun event(event: IntroEvent) = viewModelScope.launch { _eventFlow.emit(event) }
 
-    fun nonMemberLogin(){event(IntroEvent.NonMemberLogin())}
-    fun memberLogin(){event(IntroEvent.MemberLogin())}
-    fun signUp(){event(IntroEvent.SignUp())}
+    fun nonMemberLogin() = event(IntroEvent.NonMemberLogin())
+    fun memberLogin() = event(IntroEvent.MemberLogin())
+    fun signUp() = event(IntroEvent.SignUp())
 
     sealed class IntroEvent {
-        data class NonMemberLogin(val unit : Unit? = null) : IntroEvent()
-        data class MemberLogin(val unit : Unit? = null) : IntroEvent()
-        data class SignUp(val unit : Unit? = null) : IntroEvent()
+        data class NonMemberLogin(val unit: Unit? = null) : IntroEvent()
+        data class MemberLogin(val unit: Unit? = null) : IntroEvent()
+        data class SignUp(val unit: Unit? = null) : IntroEvent()
     }
 }
--- feature/setting/src/main/java/com/android/mediproject/feature/setting/SettingViewModel.kt
+++ feature/setting/src/main/java/com/android/mediproject/feature/setting/SettingViewModel.kt
@@ -4,27 +4,25 @@ import MutableEventFlow
 import androidx.lifecycle.viewModelScope
 import asEventFlow
 import com.android.mediproject.core.ui.base.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 
-@HiltViewModel
 class SettingViewModel : BaseViewModel() {
     private val _eventFlow = MutableEventFlow<SettingEvent>()
     val eventFlow = _eventFlow.asEventFlow()
 
     fun event(event: SettingEvent) = viewModelScope.launch { _eventFlow.emit(event) }
 
-    fun notice(){event(SettingEvent.Notice())}
+    fun notice() = event(SettingEvent.Notice())
     fun introduce() = event(SettingEvent.Introduce())
     fun policy() = event(SettingEvent.Policy())
     fun privacy() = event(SettingEvent.Privacy())
     fun communicate() = event(SettingEvent.Communicate())
 
     sealed class SettingEvent {
-        data class Notice(val unit : Unit? = null) : SettingEvent()
-        data class Introduce(val unit : Unit? = null) : SettingEvent()
-        data class Policy(val unit : Unit? = null) : SettingEvent()
-        data class Privacy(val unit : Unit? = null) : SettingEvent()
-        data class Communicate(val unit : Unit? = null) : SettingEvent()
+        data class Notice(val unit: Unit? = null) : SettingEvent()
+        data class Introduce(val unit: Unit? = null) : SettingEvent()
+        data class Policy(val unit: Unit? = null) : SettingEvent()
+        data class Privacy(val unit: Unit? = null) : SettingEvent()
+        data class Communicate(val unit: Unit? = null) : SettingEvent()
     }
 }
