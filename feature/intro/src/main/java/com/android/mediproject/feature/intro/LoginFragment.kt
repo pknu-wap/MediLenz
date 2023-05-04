0c5fa068c1804b6241fa17b9cc64a6d7a643163e
tgyuu-An <atg0614@gmail.com>
2023-05-04 15:29:42 +0900

    회원가입 화면 비밀번호 EditText inputType Password로 변경

--- feature/intro/src/main/java/com/android/mediproject/feature/intro/LoginFragment.kt
+++ feature/intro/src/main/java/com/android/mediproject/feature/intro/LoginFragment.kt
@@ -11,27 +11,30 @@ import dagger.hilt.android.AndroidEntryPoint
 import repeatOnStarted
 
 @AndroidEntryPoint
-class LoginFragment : BaseFragment<FragmentLoginBinding,LoginViewModel>(FragmentLoginBinding::inflate) {
+class LoginFragment :
+    BaseFragment<FragmentLoginBinding, LoginViewModel>(FragmentLoginBinding::inflate) {
     override val fragmentViewModel: LoginViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.apply{
-            viewModel = fragmentViewModel.apply{
-                repeatOnStarted { eventFlow.collect{handleEvent(it)} }
+        binding.apply {
+            viewModel = fragmentViewModel.apply {
+                repeatOnStarted { eventFlow.collect { handleEvent(it) } }
             }
         }
     }
 
-    fun handleEvent(event : LoginViewModel.LoginEvent) = when(event){
+    fun handleEvent(event: LoginViewModel.LoginEvent) = when (event) {
         is LoginViewModel.LoginEvent.Login -> {
             val id = binding.loginEmail.getValue()
             val password = binding.loginPassword.getValue()
-            log("아이디 : "+id +" 비밀번호 : "+password) }
+            log("아이디 : " + id + " 비밀번호 : " + password)
+            //Todo
+        }
 
         is LoginViewModel.LoginEvent.SignUp -> {
-            log("회원가입")
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment()) }
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment())
+        }
     }
 }
