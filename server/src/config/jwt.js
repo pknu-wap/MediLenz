73a07741c1bda25fdbbed4982ed39422d57081e9
ho-sick99 <jhmoon6807@gmail.com>
2023-05-08 17:51:52 +0900

    close #37 Refresh token, Access token 검증 기능 구현

--- server/src/config/jwt.js
+++ server/src/config/jwt.js
@@ -16,9 +16,52 @@ const createRefreshToken = (email) => {
     })
 }
 
+// resolve token
+const resolveToken = (req) => {
+    const token = req.header[AUTHORIZATION_HEADER] // extract token from http header
+    if (token && token.startsWith("Bearer ")) { // if token exist
+        return token.substring(7); // return pure token value
+    }
+    return null;
+}
+
+// verifying access token 
+const verifyAccessToken = (req, res, next) => {
+    const token = resolveToken(req); // parsing token
+    if (token) { // if token does not exist
+        console.log("token is empty or sent format is wrong")
+        return res.sendStatus(401); // 401 unauthorized
+    }
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRETKEY, (err, data) => { // verifing token
+        if (err) {
+            console.log(err)
+            res.sendStatus(403); // 403 forbidden
+        }
+        req.verifiedToken = data;
+        next();
+    })
+}
+
+// verifying refresh token 
+const verifyRefreshToken = (req, res, next) => {
+    const token = resolveToken(req); // parsing token
+    if (token) { // if token does not exist
+        console.log("token is empty or sent format is wrong")
+        return res.sendStatus(401); // 401 unauthorized
+    }
+    jwt.verify(token, process.env.JWT_REFRESH_TOKEN_SECRETKEY, (err, data) => { // verifing token
+        if (err) {
+            console.log(err)
+            res.sendStatus(403); // 403 forbidden
+        }
+        req.verifiedToken = data;
+        next();
+    })
+}
 
 module.exports = {
     createAccessToken,
     createRefreshToken,
-
+    verifyAccessToken,
+    verifyRefreshToken
 }
