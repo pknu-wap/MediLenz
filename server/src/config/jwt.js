7505479a83df659117aac06f5633f98cded8ccc4
ho-sick99 <jhmoon6807@gmail.com>
2023-05-09 14:22:34 +0900

    #22 회원가입 서비스 로직 구현 및 테스팅

--- server/src/config/jwt.js
+++ server/src/config/jwt.js
@@ -3,15 +3,15 @@ const jwt = require('jsonwebtoken');
 const AUTHORIZATION_HEADER = 'Authorization'
 
 // generate access token
-const createAccessToken = (email) => {
-    return jwt.sign({ email }, process.env.JWT_ACCESS_TOKEN_SECRETKEY, {
+const createAccessToken = (email, nickname) => {
+    return jwt.sign({ email, nickname }, process.env.JWT_ACCESS_TOKEN_SECRETKEY, {
         expiresIn: "30m"
     })
 }
 
 // generate refresh token
-const createRefreshToken = (email) => {
-    return jwt.sign({ email }, process.env.JWT_REFRESH_TOKEN_SECRETKEY, {
+const createRefreshToken = (email, nickname) => {
+    return jwt.sign({ email, nickname }, process.env.JWT_REFRESH_TOKEN_SECRETKEY, {
         expiresIn: "180d"
     })
 }
