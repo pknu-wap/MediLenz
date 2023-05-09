501bf9e13d45fce739c18a45eb9a3b1737084954
tgyuu-An <atg0614@gmail.com>
2023-05-09 20:07:27 +0900

    feat myCommnetsLists layout

---
+++ feature/comments/src/main/java/com/android/mediproject/feature/comments/mycommnetsllist/MyCommentsListViewModel.kt
@@ -0,0 +1,9 @@
+package com.android.mediproject.feature.comments.mycommnetsllist
+
+import com.android.mediproject.core.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyCommentsListViewModel @Inject constructor(): BaseViewModel() {
+}
