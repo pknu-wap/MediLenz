e03acce037e5ddd3bcde52b3df99ddeaf8acd5bd
ho-sick99 <jhmoon6807@gmail.com>
2023-05-10 13:58:10 +0900

    #21 로그인 api 생성

--- server/src/controller/user/user.ctrl.js
+++ server/src/controller/user/user.ctrl.js
@@ -1,20 +1,30 @@
 "use strict";
 
 const responseMsg = require("../../config/responseMsg");
-const { createUser } = require("../../service/userService");
+const { login, createUser } = require("../../service/userService");
 
 // GET
 const output = {
-
+    //
 }
 
 // POST
 const process = {
+    // Sign-in
+    // [POST] /user/login 
+    login: async (req, res) => {
+        const { email, password } = req.body;
+        if (!(email, password)) { // parameter check
+            res.status(400).send(responseMsg.SIGNIN_BAD_REQUEST);
+        }
+        const result = await login(email, password);
+        return res.status(result.code).send(result.response);
+    },
     // Sign-up
     // [POST] /user/register
     register: async (req, res) => {
         const { email, password, nickname } = req.body;
-        if (!(email && password && nickname)) {
+        if (!(email && password && nickname)) { // parameter check
             res.status(400).send(responseMsg.SIGNUP_BAD_REQUEST);
         }
         const result = await createUser(email, password, nickname); // create user
