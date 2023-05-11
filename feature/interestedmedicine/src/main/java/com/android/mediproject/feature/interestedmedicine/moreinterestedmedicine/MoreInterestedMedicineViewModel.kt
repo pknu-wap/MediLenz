c6e5776958ed888242a46ca1473294c679a8157d
tgyuu-An <atg0614@gmail.com>
2023-05-11 21:36:30 +0900

    #34 chore add Hilt on moreInterestedMedicineviewModel

--- feature/interestedmedicine/src/main/java/com/android/mediproject/feature/interestedmedicine/moreinterestedmedicine/MoreInterestedMedicineViewModel.kt
+++ feature/interestedmedicine/src/main/java/com/android/mediproject/feature/interestedmedicine/moreinterestedmedicine/MoreInterestedMedicineViewModel.kt
@@ -1,7 +1,10 @@
 package com.android.mediproject.feature.interestedmedicine.moreinterestedmedicine
 
 import com.android.mediproject.core.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MoreInterestedMedicineViewModel : BaseViewModel() {
+@HiltViewModel
+class MoreInterestedMedicineViewModel @Inject constructor() : BaseViewModel() {
 
 }
