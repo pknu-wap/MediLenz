032ed86cdf6f92a02fefbac617f8c75ced1c7fc0
tgyuu-An <atg0614@gmail.com>
2023-05-11 21:33:45 +0900

    #34 feature interestedMedicineDecoration

--- feature/interestedmedicine/src/main/java/com/android/mediproject/feature/interestedmedicine/MoreInterestedMedicineFragment.kt
+++ feature/interestedmedicine/src/main/java/com/android/mediproject/feature/interestedmedicine/MoreInterestedMedicineFragment.kt
@@ -3,15 +3,26 @@ package com.android.mediproject.feature.interestedmedicine
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.mediproject.core.ui.base.BaseFragment
 import com.android.mediproject.feature.interestedmedicine.databinding.FragmentMoreInterestedMedicineBinding
 
 class MoreInterestedMedicineFragment : BaseFragment<FragmentMoreInterestedMedicineBinding,MoreInterestedMedicineViewModel>(FragmentMoreInterestedMedicineBinding::inflate) {
     override val fragmentViewModel: MoreInterestedMedicineViewModel by viewModels()
+    private val moreInterestedMedicineAdapter: MoreInterestedMeidicneAdapter by lazy { MoreInterestedMeidicneAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply{
+
+            interestedMedicineListRV.apply{
+                adapter = moreInterestedMedicineAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                addItemDecoration(MyCommentsListDecoration(requireContext()))
+                addItemDecoration(DividerItemDecoration(requireContext(), 1))
+            }
+
             viewModel = fragmentViewModel.apply{
 
             }
