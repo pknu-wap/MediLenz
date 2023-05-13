f16889e2f8c4e6de2ead7ad9cd25cf9b6e2568e3
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-05-13 13:25:50 +0900

    #32 bottomnav 가시성 제어 로직 변경

--- feature/camera/src/main/java/com/android/mediproject/feature/camera/aimodel/Yolo.kt
+++ feature/camera/src/main/java/com/android/mediproject/feature/camera/aimodel/Yolo.kt
@@ -6,7 +6,7 @@ import android.view.Surface
 
 
 class Yolo {
-    external fun loadModel(mgr: AssetManager?, modelid: Int = 1, cpugpu: Int = 1): Boolean
+    external fun loadModel(mgr: AssetManager?, modelid: Int = 1, cpugpu: Int = 0): Boolean
     external fun openCamera(facing: Int): Boolean
     external fun closeCamera(): Boolean
     external fun setOutputWindow(surface: Surface?): Boolean
--- feature/camera/src/main/jni/yolo.h
+++ feature/camera/src/main/jni/yolo.h
@@ -27,9 +27,9 @@ public:
     int load(AAssetManager *mgr, const char *modeltype, int target_size, const float *mean_vals, const float *norm_vals, bool use_gpu =
     true);
 
-    int detect(const cv::Mat &rgb, std::vector <Object> &objects, float prob_threshold = 0.45f, float nms_threshold = 0.35f);
+    int detect(const cv::Mat &rgb, std::vector<Object> &objects, float prob_threshold = 0.45f, float nms_threshold = 0.4f);
 
-    int draw(cv::Mat &rgb, const std::vector <Object> &objects);
+    int draw(cv::Mat &rgb, const std::vector<Object> &objects);
 
 private:
     ncnn::Net yolo;
