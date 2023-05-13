5e4c8e616a41b124cf8dc7e5170cf34e8b819e4e
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-05-13 23:12:05 +0900

    :feature:medicine에 core:domain 의존성 추가

--- feature/medicine/build.gradle.kts
+++ feature/medicine/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:common"))
     implementation(project(":core:model"))
+    implementation(project(":core:domain"))
     implementation(project(":feature:comments"))
     implementation(project(":feature:search"))
     implementation(project(":feature:penalties"))
