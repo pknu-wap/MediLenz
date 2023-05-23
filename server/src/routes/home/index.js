d2bc9823761730fb45bbfa6a7791873aa091113b
ho-sick99 <jhmoon6807@gmail.com>
2023-05-23 21:48:22 +0900

    #78 로그인 API 명세 작성

--- server/src/routes/home/index.js
+++ server/src/routes/home/index.js
@@ -6,6 +6,12 @@ router.get("/", (req, res) => {
     res.send("hello!");
 })
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: 유저 관리 API
+ */
 const user = require("../user/userRouter");
 router.use("/user", user);
 
