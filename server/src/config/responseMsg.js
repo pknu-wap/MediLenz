59a2fc4f32da59ee448c480508d966035869d24f
ho-sick99 <jhmoon6807@gmail.com>
2023-05-23 21:58:39 +0900

    #78 반환 메세지 포맷 변경

--- server/src/config/responseMsg.js
+++ server/src/config/responseMsg.js
@@ -2,21 +2,21 @@
 
 module.exports = {
     // Success
-    SIGNIN_SUCCESS: { message: "login success" },
-    SIGNUP_SUCCESS: { message: "new user created" },
-    REISSUE_SUCCESS: { message: "reissued tokens" },
+    SIGNIN_SUCCESS: "login success",
+    SIGNUP_SUCCESS: "new user created",
+    REISSUE_SUCCESS: "reissued tokens",
 
     // Request error
-    JWT_INVALID_FORMAT: { message: "token is empty or sent format is wrong" },
-    SIGNIN_BAD_REQUEST: { message: "paramter must include 'email(string), password(string)'" },
-    SIGNIN_USER_NOT_FOUND: { message: "user not found" },
-    SIGNIN_PASSWORD_MISMATCH: { message: "password mismatch" },
-    SIGNUP_BAD_REQUEST: { message: "paramter must include 'email(string), password(string), nickname(string)'" },
+    JWT_INVALID_FORMAT: "token is empty or sent format is wrong",
+    SIGNIN_BAD_REQUEST: "paramter must include 'email(string), password(string)'",
+    SIGNIN_USER_NOT_FOUND: "user not found",
+    SIGNIN_PASSWORD_MISMATCH: "password mismatch",
+    SIGNUP_BAD_REQUEST: "paramter must include 'email(string), password(string), nickname(string)'",
 
     // Response error
     SIGNUP_DUPLICATE_PARAMETER: (parameter) => {
-        return { message: `the ${parameter} is already in use` }
+        return `the ${parameter} is already in use`
     },
-    SIGNUP_INTERNAL_SERVER_ERROR: { message: "DB Error" }
+    SIGNUP_INTERNAL_SERVER_ERROR: "DB Error"
 
 }
