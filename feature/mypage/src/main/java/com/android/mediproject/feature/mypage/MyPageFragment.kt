eee73733c44147de449dc9e7690746d33b785eb8
tgyuu-An <atg0614@gmail.com>
2023-05-27 22:56:36 +0900

    #86 add login, signUp DeepLink and link button

--- feature/mypage/src/main/java/com/android/mediproject/feature/mypage/MyPageFragment.kt
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/MyPageFragment.kt
@@ -25,5 +25,7 @@ class MyPageFragment :
     private fun handleEvent(event: MyPageViewModel.MyPageEvent) = when (event) {
         is MyPageViewModel.MyPageEvent.MyCommentsList -> findNavController().navigate("medilens://main/comments_nav/myCommentsListFragment".toUri())
         is MyPageViewModel.MyPageEvent.InterestedMedicineList -> findNavController().navigate("medilens://main/moreInterestedMedicine_nav".toUri())
+        is MyPageViewModel.MyPageEvent.Login -> findNavController().navigate("medilens://main/intro_nav/login".toUri())
+        is MyPageViewModel.MyPageEvent.SignUp -> findNavController().navigate("medilens://main/intro_nav/signUp".toUri())
     }
 }
