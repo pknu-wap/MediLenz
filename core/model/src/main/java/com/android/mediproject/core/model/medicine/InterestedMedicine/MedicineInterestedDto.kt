58fe0367133dd5e324c67f078051839b6b0b205c
tgyuu-An <atg0614@gmail.com>
2023-05-28 15:19:31 +0900

    #59 add MedicineInterstedDto 추가

---
+++ core/model/src/main/java/com/android/mediproject/core/model/medicine/InterestedMedicine/MedicineInterestedDto.kt
@@ -0,0 +1,8 @@
+package com.android.mediproject.core.model.medicine.InterestedMedicine
+
+import kotlinx.serialization.SerialName
+
+data class MedicineInterestedDto (
+    val medicineName: String,
+    val createdAt : String
+        )
