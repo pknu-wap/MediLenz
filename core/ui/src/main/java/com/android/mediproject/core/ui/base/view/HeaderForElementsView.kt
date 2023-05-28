fdba83056e75f008054b057cdbb71e5c79a51ee2
tgyuu-An <atg0614@gmail.com>
2023-05-28 22:09:01 +0900

    #59 refact HeaderView 더보기 버튼 Visiblity 로직 추가

--- core/ui/src/main/java/com/android/mediproject/core/ui/base/view/HeaderForElementsView.kt
+++ core/ui/src/main/java/com/android/mediproject/core/ui/base/view/HeaderForElementsView.kt
@@ -193,6 +193,13 @@ class HeaderForElementsView constructor(
         }
     }
 
+    fun setMoreVisiblity(boolean: Boolean){
+        when(boolean){
+            false -> moreBtnView.visibility = View.GONE
+            true -> moreBtnView.visibility = View.VISIBLE
+        }
+    }
+
 
     /**
      * 확장 버튼 클릭 시 호출
