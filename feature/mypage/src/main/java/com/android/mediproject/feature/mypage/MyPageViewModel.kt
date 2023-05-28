149ff842c3eb8a4d790ccedcddda3171a7325012
tgyuu-An <atg0614@gmail.com>
2023-05-28 14:58:52 +0900

    #59 chore MyPageViewModel Hilt Annotation 추가

--- feature/mypage/src/main/java/com/android/mediproject/feature/mypage/MyPageViewModel.kt
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/MyPageViewModel.kt
@@ -4,9 +4,12 @@ import MutableEventFlow
 import androidx.lifecycle.viewModelScope
 import asEventFlow
 import com.android.mediproject.core.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MyPageViewModel : BaseViewModel() {
+@HiltViewModel
+class MyPageViewModel @Inject constructor() : BaseViewModel() {
     private val _eventFlow = MutableEventFlow<MyPageEvent>()
     val eventFlow = _eventFlow.asEventFlow()
 
