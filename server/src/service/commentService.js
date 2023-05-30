71e10ef7373db9e8a23ef2067723edfd60384775
winocreative <winocreative@gmail.com>
2023-05-30 18:04:09 +0900

    #75 : writeComment와 deleteComment 함수 추가

--- server/src/service/commentService.js
+++ server/src/service/commentService.js
@@ -1,5 +1,40 @@
-// "use strict";
+"use strict";
 
-// const { Comment } = require("../models/index");
-
-// const writeComment = async (userId, medicineId, comment, ) => {
+const { Comment } = require("../models/index");
+const writeComment = async (userId, medicineId, comment, subOrdinationId) => {
+    const newComment = await Comment.create({
+        USERID: userId,
+        MEDICINEID: medicineId,
+        COMMENT: comment,
+        SUBORDINATIONID: subOrdinationId
+    });
+    return newComment.ID;
+}
+const deleteComment = async (commentId) => {
+    const comment = await Comment.findOne({
+        where: {
+            ID: commentId
+        }
+    });
+    if (!comment) {
+        return false;
+    }
+    if (comment.SUBORDINATIONID == 0) {
+        //delete comment with no subordination
+        await Comment.destroy({
+            where: {
+                ID: commentId
+            }
+        });
+        return true;
+    }
+    //replace content with "삭제된 댓글입니다."
+    await Comment.update({
+        COMMENT: "삭제된 댓글입니다."
+    }, {
+        where: {
+            ID: commentId
+        }
+    });
+    return true;
+}
