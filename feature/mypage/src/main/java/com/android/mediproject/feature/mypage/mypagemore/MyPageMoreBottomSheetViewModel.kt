fc6652869f0c04a2ec5f3ffe0cac1befe3e74482
tgyuu-An <atg0614@gmail.com>
2023-05-31 18:33:12 +0900

    #94 chore check -> confirm 단어 수정

--- feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreBottomSheetViewModel.kt
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreBottomSheetViewModel.kt
@@ -1,9 +1,23 @@
 package com.android.mediproject.feature.mypage.mypagemore
 
+import MutableEventFlow
+import androidx.lifecycle.viewModelScope
+import asEventFlow
 import com.android.mediproject.core.ui.base.BaseViewModel
+import com.android.mediproject.feature.mypage.MyPageViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageMoreBottomSheetViewModel @Inject constructor() : BaseViewModel() {
+    private val _eventFlow = MutableEventFlow<MyPageMoreBottomSheetEvent>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun event(event : MyPageMoreBottomSheetEvent) = viewModelScope.launch{ _eventFlow.emit(event)}
+    fun confirm() = event(MyPageMoreBottomSheetEvent.Confirm)
+
+    sealed class MyPageMoreBottomSheetEvent{
+        object Confirm : MyPageMoreBottomSheetEvent()
+    }
 }
