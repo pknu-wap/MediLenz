14112aa853e7eb1101dd71c7006cb86a8501f05a
tgyuu-An <atg0614@gmail.com>
2023-05-31 18:29:38 +0900

    #94 add MyPageMoreBottomSheetViewModel

---
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreBottomSheetViewModel.kt
@@ -0,0 +1,9 @@
+package com.android.mediproject.feature.mypage.mypagemore
+
+import com.android.mediproject.core.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageMoreBottomSheetViewModel @Inject constructor() : BaseViewModel() {
+}
