092d3f877253d75ea9c60bbba9c565616bb56fc4
tgyuu-An <atg0614@gmail.com>
2023-06-01 13:56:56 +0900

    #93 add dialog로직

--- feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogFragment.kt
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogFragment.kt
@@ -1,9 +1,14 @@
 package com.android.mediproject.feature.mypage.mypagemore
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.android.mediproject.feature.mypage.R
@@ -51,15 +56,53 @@ class MyPageMoreDialogFragment(private val flag : DialogFlag) : DialogFragment()
 
     private fun handleFlag(dialogFlag : DialogFlag) {
         when (dialogFlag) {
+            //닉네임 변경 다이얼로그
             is DialogFlag.ChangeNickName -> {
                 binding.apply {
-                    dialogSubtitle1.setTitle(getString(R.string.changeNickName))
-                    dialogSubtitle1.
+                    dialogTitleTV.text = getString(R.string.changeNickName)
+                    dialogSubtitle1.setTitle(getString(com.android.mediproject.core.ui.R.string.nickName))
+                    dialogSubtitle1.setHint(getString(com.android.mediproject.core.ui.R.string.nickNameHint))
+                    dialogSubtitle2.visibility = View.GONE
                 }
             }
 
-            is DialogFlag.ChangePassword -> {}
-            is DialogFlag.Withdrawal -> {}
+            //비밀번호 변경 다이얼로그
+            is DialogFlag.ChangePassword -> {
+                binding.apply {
+                    dialogTitleTV.text = getString(R.string.changePassword)
+                    dialogSubtitle1.setTitle(getString(com.android.mediproject.core.ui.R.string.password))
+                    dialogSubtitle1.setHint(getString(com.android.mediproject.core.ui.R.string.passwordHint))
+                    dialogSubtitle2.visibility = View.VISIBLE
+                }
+            }
+
+            //회원 탈퇴 다이얼로그
+            is DialogFlag.Withdrawal -> {
+                binding.apply {
+                    val span =
+                        SpannableStringBuilder(getString(R.string.withdrawalDescription)).apply {
+                            setSpan(
+                                ForegroundColorSpan(
+                                    ContextCompat.getColor(
+                                        requireContext(),
+                                        com.android.mediproject.core.ui.R.color.red
+                                    )
+                                ), 5, 8, Spannable.SPAN_INCLUSIVE_INCLUSIVE
+                            )
+                            setSpan(
+                                UnderlineSpan(),
+                                5,
+                                8,
+                                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+                            )
+                        }
+
+                    dialogTitleTV.text = getString(R.string.withdrawal)
+                    dialogSubtitle1.title.text = span
+                    dialogSubtitle1.setHint(getString(R.string.withdrawalHint))
+                    dialogSubtitle2.visibility = View.GONE
+                }
+            }
         }
     }
 
