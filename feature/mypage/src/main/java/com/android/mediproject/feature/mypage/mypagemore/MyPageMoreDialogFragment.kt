e52e987af33b9d1d4ca44bc83370ccaf9d4ffce6
tgyuu-An <atg0614@gmail.com>
2023-06-01 13:21:17 +0900

    #93 MyPageMoreDialog Base작업

--- feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogFragment.kt
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogFragment.kt
@@ -5,17 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import com.android.mediproject.feature.mypage.R
+import com.android.mediproject.feature.mypage.databinding.FragmentMyPageMoreDialogBinding
 import dagger.hilt.android.AndroidEntryPoint
+import repeatOnStarted
 
 @AndroidEntryPoint
-class MyPackageMoreDialogFragment : DialogFragment() {
+class MyPageMoreDialogFragment(private val dialogFlag : DialogFlag) : DialogFragment() {
+
+    sealed class DialogFlag{
+        object ChangeNickName : DialogFlag()
+        object ChangePassword : DialogFlag()
+        object Withdrawal : DialogFlag()
+    }
+
+    private val fragmentViewModel: MyPageMoreDialogViewModel by viewModels()
+    private var _binding: FragmentMyPageMoreDialogBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_package_more_dialog, container, false)
+        _binding = FragmentMyPageMoreDialogBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            viewModel = fragmentViewModel.apply {
+                viewLifecycleOwner.repeatOnStarted { eventFlow.collect { handleEvent(it) } }
+            }
+        }
+    }
+
+    private fun handleEvent(event: MyPageMoreDialogViewModel.MyPageMoreDialogEvent) = when (event) {
+        else -> {}
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogViewModel.kt
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogViewModel.kt
@@ -1,4 +1,17 @@
 package com.android.mediproject.feature.mypage.mypagemore
 
-class MyPageMoreDialogViewModel {
+import MutableEventFlow
+import androidx.lifecycle.ViewModel
+import asEventFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MyPageMoreDialogViewModel : ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<MyPageMoreDialogEvent>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    sealed class MyPageMoreDialogEvent{
+
+    }
 }
