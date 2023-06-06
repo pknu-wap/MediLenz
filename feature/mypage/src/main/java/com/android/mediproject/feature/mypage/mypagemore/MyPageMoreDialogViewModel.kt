325994c2b04057f259ec886b0adfdd550d56e91a
tgyuu-An <atg0614@gmail.com>
2023-06-07 00:58:49 +0900

    #121 myPageMoreDialog 취소버튼 로직 적용

--- feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogViewModel.kt
+++ feature/mypage/src/main/java/com/android/mediproject/feature/mypage/mypagemore/MyPageMoreDialogViewModel.kt
@@ -4,6 +4,7 @@ import MutableEventFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import asEventFlow
+import com.android.mediproject.core.domain.user.UserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,21 +12,25 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MyPageMoreDialogViewModel @Inject constructor(): ViewModel() {
+class MyPageMoreDialogViewModel @Inject constructor(private val userUseCase: UserUseCase) :
+    ViewModel() {
 
     private val _eventFlow = MutableEventFlow<MyPageMoreDialogEvent>()
     val eventFlow = _eventFlow.asEventFlow()
 
-    private val _dialogFlag = MutableStateFlow<MyPageMoreDialogFragment.DialogFlag>(MyPageMoreDialogFragment.DialogFlag.ChangeNickName)
+    private val _dialogFlag =
+        MutableStateFlow<MyPageMoreDialogFragment.DialogFlag>(MyPageMoreDialogFragment.DialogFlag.ChangeNickName)
     val dialogFlag = _dialogFlag.asStateFlow()
 
-    fun setDialogFlag(dialogFlag : MyPageMoreDialogFragment.DialogFlag){ _dialogFlag.value = dialogFlag}
+    fun setDialogFlag(dialogFlag: MyPageMoreDialogFragment.DialogFlag) {
+        _dialogFlag.value = dialogFlag
+    }
 
-    fun event(event : MyPageMoreDialogEvent) = viewModelScope.launch{ _eventFlow.emit(event) }
+    fun event(event: MyPageMoreDialogEvent) = viewModelScope.launch { _eventFlow.emit(event) }
     fun completeDialog() = event(MyPageMoreDialogEvent.CompleteDialog)
     fun cancelDialog() = event(MyPageMoreDialogEvent.CancelDialog)
 
-    sealed class MyPageMoreDialogEvent{
+    sealed class MyPageMoreDialogEvent {
         object CompleteDialog : MyPageMoreDialogEvent()
         object CancelDialog : MyPageMoreDialogEvent()
     }
