17b1b81f4daf73e360c135fae827d4e386b6eb9b
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-06-08 06:06:29 +0900

    #91 네트워크 연결 확인 다이얼로그 추가

---
+++ app/src/main/java/com/android/mediproject/NetworkStateDialogFragment.kt
@@ -0,0 +1,21 @@
+package com.android.mediproject
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NetworkStateDialogFragment : BottomSheetDialogFragment() {
+
+    private var _binding: BindingView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.modal_bottom_sheet_content, container, false)
+
+}
