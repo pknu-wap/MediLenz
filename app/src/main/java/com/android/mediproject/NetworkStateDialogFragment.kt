7a89aaa497af8ff605721138660801733c572d13
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-06-08 13:43:50 +0900

    #91 네트워크 연결 확인 다이얼로그 작업  완료

--- app/src/main/java/com/android/mediproject/NetworkStateDialogFragment.kt
+++ app/src/main/java/com/android/mediproject/NetworkStateDialogFragment.kt
@@ -4,18 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.mediproject.databinding.ViewNetworkStateBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NetworkStateDialogFragment : BottomSheetDialogFragment() {
 
-    private var _binding: BindingView
+    private var _binding: ViewNetworkStateBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.modal_bottom_sheet_content, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View = ViewNetworkStateBinding.inflate(layoutInflater, container, false).also {
+        _binding = it
+    }.root
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.okButton.setOnClickListener {
+            dismiss()
+            requireActivity().finish()
+        }
+    }
 }
