c956e64fc0c8270cbe146d3d8f2bbf1e23c0bbc0
tgyuu-An <atg0614@gmail.com>
2023-07-17 13:09:12 +0900

    #176 refactor HomeFragment 리팩토링 완료

--- feature/home/src/main/java/com/android/mediproject/feature/home/HomeFragment.kt
+++ feature/home/src/main/java/com/android/mediproject/feature/home/HomeFragment.kt
@@ -3,7 +3,6 @@ package com.android.mediproject.feature.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.android.mediproject.core.common.uiutil.SystemBarStyler
 import com.android.mediproject.core.ui.base.BaseFragment
 import com.android.mediproject.feature.comments.recentcommentlist.RecentCommentListFragment
@@ -11,6 +10,7 @@ import com.android.mediproject.feature.home.databinding.FragmentHomeBinding
 import com.android.mediproject.feature.penalties.recentpenaltylist.RecentPenaltyListFragment
 import com.android.mediproject.feature.search.recentsearchlist.RecentSearchListFragment
 import dagger.hilt.android.AndroidEntryPoint
+import repeatOnStarted
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -21,30 +21,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHo
     @Inject
     lateinit var systemBarStyler: SystemBarStyler
 
-    private fun setBarScrollEffect() = binding.apply {
-        homeBar1.bringToFront()
-        homeBar2.bringToFront()
-
-        val headerLayoutHeight = getViewHeightInPercent(binding.headerLayout, 1.2F)
-
-        scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            binding.homeBar2.alpha = (scrollY / headerLayoutHeight)
-
-            if (binding.homeBar2.alpha > 0.7) systemBarStyler.setStyle(
-                SystemBarStyler.SystemBarColor.BLACK,
-                SystemBarStyler.SystemBarColor.BLACK,
-            )
-            else systemBarStyler.setStyle(SystemBarStyler.SystemBarColor.WHITE, SystemBarStyler.SystemBarColor.BLACK)
-        }
-    }
-
-    private fun getViewHeightInPercent(view: View, percent: Float): Float {
-        return measureViewHeight(view) * percent
-    }
-
-    private fun measureViewHeight(view: View): Int {
-        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
-        return view.measuredHeight
+    companion object {
+        const val THRESHOLD = 0.7
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -54,12 +32,22 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHo
 
     private fun setBinding() {
         binding.apply {
-            viewModel = fragmentViewModel
-            fragmentViewModel.createHeaderText(getString(R.string.headerTextOnHome))
+            viewModel = fragmentViewModel.apply {
+                initHeaderText(getString(R.string.headerTextOnHome))
+                viewLifecycleOwner.apply {
+                    repeatOnStarted { eventFlow.collect { handleEvent(it) } }
+                }
+            }
         }
-        initSearchBar()
-        initChildFragments()
         setBarStyle()
+        setBarScrollEffect()
+        setFragmentResultListener()
+    }
+
+    private fun handleEvent(event: HomeViewModel.HomeEvent) {
+        when (event) {
+            is HomeViewModel.HomeEvent.NavigateToSearch -> navigateWithNavDirections(HomeFragmentDirections.actionHomeFragmentToSearchMedicinesFragment())
+        }
     }
 
     private fun setBarStyle() {
@@ -73,33 +61,69 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHo
         )
     }
 
-    private fun initSearchBar() {
-        binding.searchView.setOnBarClickListener {
-            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSearchMedicinesFragment())
+    private fun setBarScrollEffect() = binding.apply {
+        homeBar1.bringToFront()
+        homeBar2.bringToFront()
+
+        val headerLayoutHeight = getViewHeightInPercent(binding.headerLayout, 1.2F)
+
+        scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            homeBar2.alpha = (scrollY / headerLayoutHeight)
+            setScrollBarStyle()
         }
     }
 
-    private fun initChildFragments() {
-        childFragmentManager.apply {
-            setFragmentResultListener(RecentCommentListFragment.ResultKey.RESULT_KEY.name, viewLifecycleOwner) { _, bundle ->
-                bundle.apply {
-                    val result = getInt(RecentCommentListFragment.ResultKey.WORD.name)
-                }
+    private fun getViewHeightInPercent(view: View, percent: Float): Float {
+        return measureViewHeight(view) * percent
+    }
+
+    private fun measureViewHeight(view: View): Int {
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        return view.measuredHeight
+    }
+
+    private fun setScrollBarStyle() = binding.apply {
+        if (homeBar2.alpha > THRESHOLD) {
+            systemBarStyler.setStyle(
+                SystemBarStyler.SystemBarColor.BLACK,
+                SystemBarStyler.SystemBarColor.BLACK,
+            )
+        } else {
+            systemBarStyler.setStyle(SystemBarStyler.SystemBarColor.WHITE, SystemBarStyler.SystemBarColor.BLACK)
+        }
+    }
+
+    private fun setFragmentResultListener() {
+        setRecentCommentResultLisnter()
+        setRecentPenaltyResultLisnter()
+        setRecentSearchResultLisnter()
+    }
+
+    private fun setRecentCommentResultLisnter() {
+        childFragmentManager.setFragmentResultListener(RecentCommentListFragment.ResultKey.RESULT_KEY.name, viewLifecycleOwner) { _, bundle ->
+            bundle.apply {
+                val result = getInt(RecentCommentListFragment.ResultKey.WORD.name)
             }
-            setFragmentResultListener(RecentPenaltyListFragment.ResultKey.RESULT_KEY.name, viewLifecycleOwner) { _, bundle ->
-                bundle.apply {
-                    val result = getInt(RecentPenaltyListFragment.ResultKey.PENALTY_ID.name)
-                }
+        }
+    }
+
+    private fun setRecentPenaltyResultLisnter() {
+        childFragmentManager.setFragmentResultListener(RecentPenaltyListFragment.ResultKey.RESULT_KEY.name, viewLifecycleOwner) { _, bundle ->
+            bundle.apply {
+                val result = getInt(RecentPenaltyListFragment.ResultKey.PENALTY_ID.name)
             }
-            setFragmentResultListener(RecentSearchListFragment.ResultKey.RESULT_KEY.name, viewLifecycleOwner) { _, bundle ->
-                findNavController().navigate(
-                    HomeFragmentDirections.actionHomeFragmentToSearchMedicinesFragment(
-                        bundle.getString(
-                            RecentSearchListFragment.ResultKey.WORD.name,
-                        ) ?: "",
-                    ),
-                )
-            }
+        }
+    }
+
+    private fun setRecentSearchResultLisnter() {
+        childFragmentManager.setFragmentResultListener(RecentSearchListFragment.ResultKey.RESULT_KEY.name, viewLifecycleOwner) { _, bundle ->
+            navigateWithNavDirections(
+                HomeFragmentDirections.actionHomeFragmentToSearchMedicinesFragment(
+                    bundle.getString(
+                        RecentSearchListFragment.ResultKey.WORD.name,
+                    ) ?: "",
+                ),
+            )
         }
     }
 }
