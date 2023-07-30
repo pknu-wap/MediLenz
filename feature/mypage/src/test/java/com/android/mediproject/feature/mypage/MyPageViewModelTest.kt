5c819964b94348d20523b19d6efec6b28aaf388e
tgyuu-An <atg0614@gmail.com>
2023-07-31 01:30:46 +0900

    단위테스트 종속성 추가

---
+++ feature/mypage/src/test/java/com/android/mediproject/feature/mypage/MyPageViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.android.mediproject.feature.mypage
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+
+
+class MyPageViewModelTest {
+
+    private lateinit var viewModel : MyPageViewModel
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+}
