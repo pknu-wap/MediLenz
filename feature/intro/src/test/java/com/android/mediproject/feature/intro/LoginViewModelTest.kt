26252e60b0360ba6b73af878399b8b339c6c17dc
tgyuu-An <atg0614@gmail.com>
2023-08-06 17:50:17 +0900

    LoginViewModel 단위테스트 완료

--- feature/intro/src/test/java/com/android/mediproject/feature/intro/LoginViewModelTest.kt
+++ feature/intro/src/test/java/com/android/mediproject/feature/intro/LoginViewModelTest.kt
@@ -10,6 +10,9 @@ import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
 
 class LoginViewModelTest {
 
@@ -28,7 +31,7 @@ class LoginViewModelTest {
     }
 
     @Test
-    fun `로그인 시 이메일 형식이 맞지 않으면 RegexError를 반환한다`() {
+    fun `로그인 시 이메일 형식이 아니면 RegexError를 반환한다`() {
         //given
         val notValidEmail = "example@.com"
         val validPassword = "abcd123456"
@@ -39,5 +42,47 @@ class LoginViewModelTest {
         //then
         val result = viewModel.loginState.value
         assertThat(result).isEqualTo(LoginViewModel.LoginState.RegexError)
+    }
+
+    @Test
+    fun `로그인 시 비밀번호가 3글자 이하일 경우 RegexError를 반환한다`() {
+        //given
+        val validEmail = "wap@gmail.com"
+        val notValidPassword = "a12"
+
+        //when
+        viewModel.loginWithCheckRegex(validEmail, notValidPassword, false)
+
+        //then
+        val result = viewModel.loginState.value
+        assertThat(result).isEqualTo(LoginViewModel.LoginState.RegexError)
+    }
+
+    @Test
+    fun `로그인 시 비밀번호가 17글자 이상일 경우 RegexError를 반환한다`() {
+        //given
+        val validEmail = "wap@gmail.com"
+        val notValidPassword = "123456789abcdefghijklmn"
+
+        //when
+        viewModel.loginWithCheckRegex(validEmail, notValidPassword, false)
+
+        //then
+        val result = viewModel.loginState.value
+        assertThat(result).isEqualTo(LoginViewModel.LoginState.RegexError)
+    }
+
+    @Test
+    fun `로그인 시 아이디가 이메일 형식이고, 비밀번호가 4글자 이상, 16글자 이하일 경우 Success를 반환한다`() {
+        //given
+        val validEmail = "wap@gmail.com"
+        val validPassword = "wapMediLenz2023"
+
+        //when
+        viewModel.loginWithCheckRegex(validEmail, validPassword, false)
+
+        //then
+        val result = viewModel.loginState.value
+        assertThat(result).isEqualTo(LoginViewModel.LoginState.LoginSuccess)
     }
 }
