6dd1bde6bfb0e75c5681c9a349e3c402736fd1b0
tgyuu-An <atg0614@gmail.com>
2023-08-09 07:35:13 +0900

    #204 MyPageFragment 함수 위치 조정

--- feature/comments/src/main/java/com/android/mediproject/feature/comments/mycommentslist/MyCommentsListAdapter.kt
+++ feature/comments/src/main/java/com/android/mediproject/feature/comments/mycommentslist/MyCommentsListAdapter.kt
@@ -5,37 +5,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.android.mediproject.core.model.comments.CommentListResponse
 import com.android.mediproject.feature.comments.databinding.ItemMyCommentBinding
 
 class MyCommentsViewHolder(
     private val binding: ItemMyCommentBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    init {
-        binding.apply {
-            root.setOnClickListener {
-                myComment?.apply {
-                    onClick?.invoke(this)
-                }
-            }
-        }
-    }
-
-    fun bind(myComment: MyComment) {
-        binding.myComment = myComment
+    fun bind(comment: CommentListResponse.Comment) {
+        binding.comment = comment
     }
 }
 
 
-class MyCommentsListAdapter : ListAdapter<MyComment, MyCommentsViewHolder>(diffUtil) {
+class MyCommentsListAdapter : ListAdapter<CommentListResponse.Comment, MyCommentsViewHolder>(diffUtil) {
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<MyComment>() {
-            override fun areItemsTheSame(oldItem: MyComment, newItem: MyComment): Boolean {
+        val diffUtil = object : DiffUtil.ItemCallback<CommentListResponse.Comment>() {
+            override fun areItemsTheSame(oldItem: CommentListResponse.Comment, newItem: CommentListResponse.Comment): Boolean {
                 return oldItem === newItem
             }
 
-            override fun areContentsTheSame(oldItem: MyComment, newItem: MyComment): Boolean {
+            override fun areContentsTheSame(oldItem: CommentListResponse.Comment, newItem: CommentListResponse.Comment): Boolean {
                 return oldItem == newItem
             }
         }
@@ -51,7 +42,7 @@ class MyCommentsListAdapter : ListAdapter<MyComment, MyCommentsViewHolder>(diffU
         holder.bind(getItem(position))
     }
 
-    override fun submitList(list: MutableList<MyComment>?) {
+    override fun submitList(list: MutableList<CommentListResponse.Comment>?) {
         super.submitList(list)
     }
 }
