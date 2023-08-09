242770654c26cdeed07e4cc4340480f07bf45e88
tgyuu-An <atg0614@gmail.com>
2023-08-09 15:30:19 +0900

    #207 token 변수 stateFlow -> sharedFlow로 변경

--- feature/favoritemedicine/src/main/java/com/android/mediproject/feature/favoritemedicine/favoritemedicinemore/FavoriteMedicineMoreViewModel.kt
+++ feature/favoritemedicine/src/main/java/com/android/mediproject/feature/favoritemedicine/favoritemedicinemore/FavoriteMedicineMoreViewModel.kt
@@ -8,7 +8,10 @@ import com.android.mediproject.core.model.token.CurrentTokens
 import com.android.mediproject.core.model.token.TokenState
 import com.android.mediproject.core.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,11 +25,14 @@ class FavoriteMedicineMoreViewModel @Inject constructor(
     private val _favoriteMedicineList = MutableStateFlow<List<FavoriteMedicineMoreInfo>>(listOf())
     val favoriteMedicineList get() = _favoriteMedicineList
 
-    private val _token = MutableStateFlow<TokenState<CurrentTokens>>(TokenState.Empty)
-    val token get() = _token.asStateFlow()
+    private val _token = MutableSharedFlow<TokenState<CurrentTokens>>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+    val token get() = _token.asSharedFlow()
 
-    fun loadTokens() = viewModelScope.launch { getTokenUseCase().collect { _token.value = it } }
-
+    fun loadTokens() = viewModelScope.launch {
+        getTokenUseCase().collect {
+            _token.emit(it)
+        }
+    }
     fun loadFavoriteMedicines() =
         viewModelScope.launch {
             getFavoriteMedicineUseCase.getFavoriteMedicineMoreList()
